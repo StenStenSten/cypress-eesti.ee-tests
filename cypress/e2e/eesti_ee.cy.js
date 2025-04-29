7d1fcc704a66134f96c484c1a8cdc7f98342f9e1
StenStenSten <yolosten@gmail.com>
2025-04-29 22:58:05 +0300

    done all

--- cypress/e2e/eesti_ee.cy.js
+++ cypress/e2e/eesti_ee.cy.js
@@ -1,81 +1,85 @@
 describe('1. Avalehe testid', () => {
-    beforeEach(() => {
-      cy.visit('/')
-    })
-  
-    it('Leht laeb edukalt', () => {
-      cy.title().should('include', 'Eesti.ee')
-    })
-  
-    it('Otsinguriba on nähtav ja funktsionaalne', () => {
-      cy.get('input[type="search"]').should('be.visible')
-    })
-  
-    it('Peamenüü punktid eksisteerivad', () => {
-      cy.contains('Eraisik').should('exist')
-      cy.contains('Ettevõtja').should('exist')
-      cy.contains('Ametnik').should('exist')
-    })
+  beforeEach(() => {
+    cy.visit('/')
   })
-  
-  describe('2. Otsingu testid', () => {
-    beforeEach(() => {
-      cy.visit('/')
-    })
-  
-    it('Otsib "Eesti hümn" ja leiab tulemused', () => {
-      cy.get('input[type="search"]').type('Eesti hümn{enter}')
-      cy.contains('Eesti Vabariigi hümn').should('exist')
-    })
-  
-    it('Tühi otsing kuvab veateate', () => {
-      cy.get('input[type="search"]').type('{enter}')
-      cy.contains('Palun sisesta otsingusõna').should('exist') 
-    })
+
+  it('Leht laeb edukalt', () => {
+    cy.title().should('include', 'Eesti.ee')  
   })
-  
-  describe('3. Teenuste navigeerimine', () => {
-    beforeEach(() => {
-      cy.visit('/')
-    })
-  
-    it('"Tervis ja retseptid" avamine "Eraisiku" alt', () => {
-      cy.contains('Eraisik').click()
-      cy.contains('Tervis ja retseptid').click()
-      cy.contains('Retseptid').should('exist')
-    })
-  
-    it('"Retseptid" leht laeb korrektselt', () => {
-      cy.contains('Retseptid').click()
-      cy.url().should('include', 'retseptid')
-      cy.contains('Retsepti info').should('exist') 
-    })
-  
-    it('Terviseportaal avaneb uues aknas', () => {
-      cy.contains('Terviseportaal').should('have.attr', 'target', '_blank')
-    })
+
+  it('Otsingukast on nähtav ja funktsionaalne', () => {
+    cy.get('input[type="text"]', { timeout: 10000 }).should('be.visible')  
   })
-  
-  describe('4. Kontaktilehe testid', () => {
-    beforeEach(() => {
-      cy.visit('/')
-    })
-  
-    it('"Võtke meiega ühendust" leht avaneb', () => {
-      cy.contains('Võtke meiega ühendust').click()
-      cy.url().should('include', 'kontakt')
-    })
-  
-    it('Kontaktivorm sisaldab olulisi välju', () => {
-      cy.get('input[name="name"]').should('exist')
-      cy.get('input[name="email"]').should('exist')
-      cy.get('textarea[name="message"]').should('exist')
-    })
-  
-    it('Vigane e-post annab veateate', () => {
-      cy.get('input[name="email"]').type('viganeemail')
-      cy.get('form').submit()
-      cy.contains('Palun sisesta korrektne e-posti aadress').should('exist')
-    })
+
+  it('Peamenüü punktid "Eraisik" ja "Ettevõtja" eksisteerivad', () => {
+    cy.contains('Eraisik').should('be.visible')
+    cy.contains('Ettevõtja').should('be.visible')  
   })
-  
+})
+
+describe('2. Otsingu testid', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('Otsib "Eesti hümn" ja leiab tulemused', () => {
+    cy.get('input[type="text"]').should('be.visible').type('Eesti hümn{enter}')
+    cy.contains('Eesti Vabariigi hümn', { timeout: 10000 }).should('exist')  
+  })
+
+  it('Tühi otsing kuvab veateate', () => {
+    cy.get('input[type="text"]').should('be.visible').clear().type('{enter}')  
+    cy.contains('Palun sisesta otsingusõna', { timeout: 5000 }).should('exist')  
+  })
+})
+
+describe('3. Teenuste navigeerimine', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('"Tervis ja retseptid" avamine "Eraisiku" alt', () => {
+    cy.contains('Eraisik').click()
+    cy.contains('Tervis ja retseptid').scrollIntoView().click({ force: true })  
+    cy.contains('Retseptid', { timeout: 10000 }).should('exist')  
+  })
+
+  it('"Retseptid" leht laeb korrektselt', () => {
+    cy.contains('Eraisik').click()
+    cy.contains('Tervis ja retseptid').click({ force: true })
+    cy.contains('Retseptid').click({ force: true })
+    cy.url().should('include', 'retseptid')  
+    cy.contains('Retsepti info', { timeout: 10000 }).should('exist')  
+  })
+
+  it('Terviseportaal avaneb uues aknas', () => {
+    cy.contains('Terviseportaal').should('have.attr', 'target', '_blank')  
+  })
+})
+
+describe('4. Kontaktilehe testid', () => {
+  beforeEach(() => {
+    cy.visit('https://www.eesti.ee/eraisik/et/vajad-abi')  
+  })
+
+  it('"Võtke meiega ühendust" nupp töötab ja viib abilehele', () => {
+    cy.contains('Võtke meiega ühendust').scrollIntoView().click({ force: true })  
+    cy.url().should('include', 'kontakt')  
+    cy.contains('Abi ja kontakt', { timeout: 10000 }).should('exist')  
+  })
+
+  it('Kontaktivorm sisaldab olulisi välju', () => {
+    cy.get('input[name="name"]', { timeout: 10000 }).should('exist')  
+    cy.get('input[name="email"]').should('exist')  
+    cy.get('select[name="role"]').should('exist')  
+    cy.get('select[name="service"]').should('exist')  
+    cy.get('select[name="obstacle"]').should('exist')  
+    cy.get('textarea[name="description"]').should('exist')  
+  })
+
+  it('Vigane e-post annab veateate', () => {
+    cy.get('input[name="email"]').type('viganeemail')  
+    cy.get('form').submit()  
+    cy.contains('Palun sisesta korrektne e-posti aadress', { timeout: 5000 }).should('exist')  
+  })
+})
--- report.md
+++ report.md
@@ -1,4 +1,53 @@
-Avalehe testid – edukalt laetud ja menüü toimib
-Otsing töötab, tulemused on asjakohased, tühi otsing annab veateate
-Teenuste navigatsioon toimib, retseptide leht ja link kontrollitud
-Kontaktivorm olemas, vigase e-postiga valideerimine toimib
+✅ Õnnestunud testid (6/11)
+Avalehe laadimine
+Kontrolliti, et Eesti.ee avaleht laeb edukalt ja pealkiri sisaldab "Eesti.ee".
+
+Otsingukasti olemasolu ja nähtavus
+Veenduti, et otsinguväli on nähtav ja kasutatav.
+
+Peamenüüpunktid
+Kontrolliti, et peamenüüs on nähtavad lingid "Eraisik" ja "Ettevõtja".
+
+"Tervis ja retseptid" avamine "Eraisiku" alt
+Kontrolliti, et kategooria on nähtav ja "Retseptid" on alateenusena olemas.
+
+Terviseportaali link avab uue akna
+Testitud, et "Terviseportaal" lingil on target="_blank" atribuut.
+
+"Eesti hümn" otsing töötab
+Kontrolliti, et märksõna "Eesti hümn" tagastab asjakohased tulemused.
+
+
+
+❌ Ebaõnnestunud testid (5/11)
+Tühi otsing ei kuva veateadet
+
+Ootus: Kuva tekst "Palun sisesta otsingusõna".
+
+Põhjus: Veateade ei ilmunud või pole üldse olemas (võib viidata puudulikule kasutajaliidese valideerimisele).
+
+"Retseptid" leht ei laadinud korrektselt
+
+Ootus: Tekst "Retsepti info" pidi olema lehel.
+
+Põhjus: Leht kas ei jõudnud laadida või tekst on erinev / eemaldatud.
+
+"Võtke meiega ühendust" link ei töötanud ootuspäraselt
+
+Ootus: URL pidi sisaldama kontakt.
+
+Põhjus: Pärast nupule vajutamist jäi leht samaks (/vajad-abi).
+
+Kontaktivormi väljad puuduvad või pole tuvastatavad
+
+Ootus: Sisendväljad nagu input[name="name"], email, textarea[name="description"].
+
+Põhjus: Elemendid puuduvad või nende nimeatribuudid on erinevad võrreldes testiga.
+
+Vigase e-posti sisestamisel ei kuvata veateadet
+
+Ootus: Eeldati valideerimisveateadet valel e-postil.
+
+Põhjus: Vorm kas ei valideerinud sisendit või test ei jõudnud õigesse seisundisse.
+
+
